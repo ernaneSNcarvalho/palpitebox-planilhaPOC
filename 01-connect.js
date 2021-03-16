66a2c2d754bbbcd2c680f5c4cb048708377f2f7c
POC <ernanecarvalho89@gmail.com>
2021-03-16 19:47:25 -0300

    Criação planilha POC

---
+++ 01-connect.js
@@ -0,0 +1,12 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const credentials = require('./credentials.json')
+
+const doc = new GoogleSpreadsheet('1ieTUEtcdPPVF0-vgiD7eV-7jeRcBVtiqCkLBjMsHUTo')
+
+const run = async() => {
+    await doc.useServiceAccountAuth(credentials)
+    await doc.loadInfo()
+    console.log(doc.title)
+}
+
+run()
